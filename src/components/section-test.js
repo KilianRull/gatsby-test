94abfeb608ce3ff2e3d3879ad67263636cc370c5
Kilian Rullkötter <kilian@kjubo.com>
2020-02-13 00:20:56 +0100

    testing styled components - beginning

---
+++ src/components/section-test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const SectionTest = props => (
+    <SectionGroup>
+        <SectionTitleGroup>
+            <SectionText>
+
+            </SectionText>
+        </SectionTitleGroup>
+
+    </SectionGroup>
+)
+
+export default SectionTest
